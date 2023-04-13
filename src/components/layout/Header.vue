b3f6e33ba3134705573ec842626d89981844fc96
P4P4N1CK <90088607+P4P4N1CK@users.noreply.github.com>
2023-04-13 08:51:17 +0700

    Initial commit

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header container">
+    <h1>abr player</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style scoped>
+h1 {
+  font-size: 3rem;
+  text-align: left;
+}
+.logo {
+  width: 48px;
+}
+</style>
